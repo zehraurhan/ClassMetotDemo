d905aa3f1cd9ce93491644d5b82ce8c3dd2b3845
zehra urhan <zhrurhn1409@hotmail.com>
2021-02-27 22:03:21 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add( Customer customer ) 
+        {
+            Console.WriteLine("Customer added" + " :" + customer.CustomerName);
+        }
+
+        public void Delete (Customer customer)
+        {
+            Console.WriteLine("Customer deleted" + " :" + customer.CustomerName);
+        }
+
+        public void List( Customer customer)
+        {
+            Console.WriteLine("Customer listed" + " :" + customer.CustomerName);
+        }
+    }
+
+
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer customer1 = new Customer();
+            customer1.CustomerId = 1;
+            customer1.CustomerName = "Tuba";
+            customer1.CustomerLastName = "Akay";
+            customer1.CustomerAge = 18;
+
+            Customer customer2 = new Customer();
+            customer2.CustomerId = 2;
+            customer2.CustomerName = "Ahmet";
+            customer2.CustomerLastName = "Aka";
+            customer2 .CustomerAge = 19;
+
+            CustomerManager customermanager = new CustomerManager();
+            customermanager.Add(customer1);
+            customermanager.Delete(customer2);
+
+
+
+
+
+
+        }
+    }
+}
